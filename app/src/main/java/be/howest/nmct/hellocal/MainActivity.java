074d122314908b62362958ce8cdc5e3f080958da
Arno Vande Cappelle <arnovdc@msn.com>
2016-11-07 10:13:33 +0100

    SearchButtonDoenWerken

--- app/src/main/java/be/howest/nmct/hellocal/MainActivity.java
+++ app/src/main/java/be/howest/nmct/hellocal/MainActivity.java
@@ -1,10 +1,9 @@
 package be.howest.nmct.hellocal;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,12 +12,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import be.howest.nmct.hellocal.auth.AuthHelper;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, InfoFragment.OnFragmentInteractionListener {
 
     NavigationView navigationView = null;
     Toolbar toolbar = null;
@@ -163,5 +161,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
--- app/src/main/java/be/howest/nmct/hellocal/SearchFragment.java
+++ app/src/main/java/be/howest/nmct/hellocal/SearchFragment.java
@@ -3,6 +3,7 @@ package be.howest.nmct.hellocal;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.widget.Button;
 
 public class SearchFragment extends Fragment {
 
+    private Button buttonSearch;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -20,7 +22,26 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        buttonSearch = (Button) view.findViewById(R.id.btnSearch);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Search();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return  inflater.inflate(R.layout.fragment_search, container, false);
+        return  view;
+
+    }
+
+    private void Search(){
+        final FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment fragment = new InfoFragment();
+        ft.replace(R.id.fragmentContainer, fragment, "NewFragmentTag");
+        ft.commit();
     }
 }
