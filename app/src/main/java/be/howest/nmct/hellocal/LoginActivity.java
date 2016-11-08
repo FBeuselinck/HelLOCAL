4403d5b0c30c4e545139f957b23a835742d5bbc4
Arno Vande Cappelle <arnovdc@msn.com>
2016-11-08 19:05:20 +0100

    SignUp Functie

--- app/src/main/java/be/howest/nmct/hellocal/LoginActivity.java
+++ app/src/main/java/be/howest/nmct/hellocal/LoginActivity.java
@@ -1,214 +1,107 @@
 package be.howest.nmct.hellocal;
 
-import android.accounts.Account;
-import android.accounts.AccountAuthenticatorResponse;
-import android.accounts.AccountManager;
-import android.accounts.OnAccountsUpdateListener;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatEditText;
+import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import be.howest.nmct.hellocal.R;
-import be.howest.nmct.hellocal.auth.AuthHelper;
-import be.howest.nmct.hellocal.auth.Contract;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements
         View.OnClickListener {
 
+    private EditText editTextEmail, editTextPassword;
+
     private static final String TAG = "LoginActivity";
 
-    /* Keys for persisting instance variables in savedInstanceState */
-    private static final String KEY_IS_RESOLVING = "is_resolving";
-    private static final String KEY_SHOULD_RESOLVE = "should_resolve";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
-
-    /* View to display current status (signed-in, signed-out, disconnected, etc) */
-    private TextView mStatus;
-
-    // [START resolution_variables]
-    /* Is there a ConnectionResult resolution in progress? */
-    private boolean mIsResolving = false;
-
-    /* Should we automatically resolve ConnectionResults when possible? */
-    private boolean mShouldResolve = false;
-
-    private AppCompatEditText editTextUseremail;
-    private AppCompatEditText editTextPassword;
-    private LinearLayout linearLayout;
-    private String token = "";
-
-    private AccountManager mAccountManager;
-    private AccountAuthenticatorResponse mAccountAuthenticatorResponse;
-    private String mFullName;
-    private String mEmail;
-
-    //listener voor AccountManager -> update UI
-    private OnAccountsUpdateListener onAccountsUpdateListener;
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Set up view instances
-        mStatus = (TextView) findViewById(R.id.status);
-        editTextUseremail = (AppCompatEditText) findViewById(R.id.editTextUseremail);
-        editTextPassword = (AppCompatEditText) findViewById(R.id.edittextPassword);
-        linearLayout = (LinearLayout) findViewById(R.id.linearlayout1);
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
-        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        editTextEmail = (EditText) findViewById(R.id.editTextUseremail);
+        editTextPassword = (EditText) findViewById(R.id.edittextPassword);
 
-        mAccountManager = AccountManager.get(this);
-        mAccountAuthenticatorResponse = this.getIntent().getParcelableExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE);
-        if (mAccountAuthenticatorResponse != null) {
-            mAccountAuthenticatorResponse.onRequestContinued();
-        }
-
-
-        if (savedInstanceState != null) {
-            mIsResolving = savedInstanceState.getBoolean(KEY_IS_RESOLVING);
-            mShouldResolve = savedInstanceState.getBoolean(KEY_SHOULD_RESOLVE);
-        }
-
-        updateUI(AuthHelper.isUserLoggedIn(this));
-    }
-
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        onAccountsUpdateListener = new OnAccountsUpdateListener() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onAccountsUpdated(Account[] accounts) {
-                updateUI(AuthHelper.isUserLoggedIn(LoginActivity.this));
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    ChangeActivity();
+                }
             }
         };
-
-        mAccountManager.addOnAccountsUpdatedListener(onAccountsUpdateListener, null, true);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (onAccountsUpdateListener != null)
-            mAccountManager.removeOnAccountsUpdatedListener(onAccountsUpdateListener);
+    private void ChangeActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class );
+        startActivity(intent);
     }
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.sign_in_button:
-                onSignInClicked();
+                SignIn();
                 break;
             case R.id.sign_out_button:
-                onSignOut();
+                SignUp();
                 break;
         }
     }
 
-    private void onSignInClicked() {
-        mEmail = editTextUseremail.getText().toString();
-        String passwordString = editTextPassword.getText().toString();
-        addAccount(mEmail);
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
-
-    private void onSignOut() {
-        AuthHelper.logUserOff(this);
-
-        Snackbar snackbar = Snackbar
-                .make(linearLayout, "Logged out successfully", Snackbar.LENGTH_LONG)
-                .setAction("Close app?", new View.OnClickListener() {
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
+    }
+    private void  SignUp()
+    {
+        mAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onClick(View view) {
-                        ActivityCompat.finishAffinity(LoginActivity.this);
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "MY MESSAGE LOGIN FAILED",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(LoginActivity.this, "MY MESSAGE REGISTERED",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
-        snackbar.show();
+    }
+    private void SignIn()
+    {
 
     }
 
-
-    private void updateUI(boolean isSignedIn) {
-        if (isSignedIn) {
-            mStatus.setVisibility(View.VISIBLE);
-            mStatus.setText(AuthHelper.getUsername(this) + " is logged in.");
-            editTextUseremail.setVisibility(View.GONE);
-            editTextPassword.setVisibility(View.GONE);
-            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-            findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
-        } else {
-            // Show signed-out message and clear email field
-            mStatus.setText(R.string.signed_out);
-            mStatus.setVisibility(View.GONE);
-            editTextUseremail.setVisibility(View.VISIBLE);
-            editTextPassword.setVisibility(View.VISIBLE);
-            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
-
-            editTextUseremail.setText("");
-            editTextPassword.setText("");
-            mStatus.setText("");
-        }
-    }
-
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(KEY_IS_RESOLVING, mIsResolving);
-        outState.putBoolean(KEY_SHOULD_RESOLVE, mShouldResolve);
-    }
-
-
-    private void addAccount(String userNameString) {
-        Account[] accountsByType = mAccountManager.getAccountsByType(Contract.ACCOUNT_TYPE);
-        Account account;
-        if (accountsByType.length == 0) {
-            // nog geen account aanwezig
-            account = new Account(userNameString, Contract.ACCOUNT_TYPE);
-            mAccountManager.addAccountExplicitly(account, null, null);
-        } else if (!userNameString.equals(accountsByType[0].name)) {
-            // er bestaat reeds een account met andere naam
-            mAccountManager.removeAccount(accountsByType[0], this, null, null);
-            account = new Account(userNameString, Contract.ACCOUNT_TYPE);
-            mAccountManager.addAccountExplicitly(account, null, null);
-        } else {
-            // account met de zelfde username terug gevonden
-            account = accountsByType[0];
-        }
-
-        Intent intent = new Intent();
-        intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, userNameString);
-        intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, Contract.ACCOUNT_TYPE);
-
-        if (mAccountAuthenticatorResponse != null) {
-            Bundle bundle = intent.getExtras();
-            bundle.putString(AccountManager.KEY_ACCOUNT_NAME, userNameString);
-            bundle.putString(AccountManager.KEY_ACCOUNT_TYPE, Contract.ACCOUNT_TYPE);
-            mAccountAuthenticatorResponse.onResult(bundle);
-        }
-
-        setResult(RESULT_OK, intent);
-        finish();
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        //als user niet meer ingelogd is, en de gebruiker drukt op back-button ~> app laten afsluiten
-        if (!AuthHelper.isUserLoggedIn(this)) {
-            ActivityCompat.finishAffinity(LoginActivity.this);
-        }
-    }
 }
 
