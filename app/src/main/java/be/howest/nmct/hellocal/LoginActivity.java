aabf8892bbd78b2f8ce92349278c432446d712e5
Freek Beuselinck <freek.beuselinck@student.howest.be>
2016-11-14 10:25:11 +0100

    Custom font added

--- app/src/main/java/be/howest/nmct/hellocal/LoginActivity.java
+++ app/src/main/java/be/howest/nmct/hellocal/LoginActivity.java
@@ -1,12 +1,14 @@
 package be.howest.nmct.hellocal;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -27,6 +29,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView tx = (TextView)findViewById(R.id.appTitle);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Amethyst.ttf");
+        tx.setTypeface(custom_font);
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
         findViewById(R.id.sign_up_button).setOnClickListener(this);
